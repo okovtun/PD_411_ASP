c51603c0e4232a25313a65432e85eb8f0a2289c6
User <User@DESKTOP-QHG18FL>
2026-03-21 17:01:47 +0300

    Movie details & URL

---
+++ Blazor/Movies/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Movies.Models
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(50, MinimumLength = 3)]
+		public string Title { get; set; }
+
+		[RangeAttribute(typeof(DateOnly), "1888-10-14", "9999-12-31")]
+		public DateOnly ReleaseDate { get; set; }
+
+		public string Genre { get; set; }
+		public string? URL { get; set; }
+	}
+}
