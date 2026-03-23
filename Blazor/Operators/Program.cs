185472c1a85b1833a8a1923fc5f112df5bd824e8
User <User@DESKTOP-QHG18FL>
2026-03-23 16:36:20 +0300

    Search Movies DONE

---
+++ Blazor/Operators/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] words =
+{
+	"Красота", "среди", "бегущих", null, "Первых", "нет", "и", "отстающих"
+};
+
+for (int i = 0; i < words.Length; i++)
+{
+	Console.Write($"{words[i]} ");
+}
+Console.WriteLine(null);
