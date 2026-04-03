e3b6728cecdb6a76ad690e14c1aae56d72cfeac6
User <User@DESKTOP-QHG18FL>
2026-04-03 16:38:05 +0300

    Weekdays DONE

---
+++ Blazor/Academy2/Components/Pages/GroupPages/Week.cs
@@ -0,0 +1,21 @@
+﻿namespace Academy2.Components.Pages.GroupPages
+{
+	public class Week
+	{
+		readonly string[] DAYNAMES = { "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс" };
+		int days;
+		public Week(int days)
+		{
+			this.days = days;
+		}
+		public override string ToString()
+		{
+			List<string> result = new List<string>();
+			for (int i = 0; i < 7; i++)
+			{
+				if (((days >> i) & 1) != 0) result.Add(DAYNAMES[i]);
+			}
+			return string.Join(", ", result);
+		}
+	}
+}
