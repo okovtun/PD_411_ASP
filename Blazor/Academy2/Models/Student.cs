f229c6654a3692034cc48736122081dbdb3e2139
User <User@DESKTOP-QHG18FL>
2026-04-08 17:51:15 +0300

    Photo start

--- Blazor/Academy2/Models/Student.cs
+++ Blazor/Academy2/Models/Student.cs
@@ -3,21 +3,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Academy2.Models
 {
-	public class Student
+	public class Student:Human
 	{
 		[Key]
 		public int stud_id { get; set; }
-		[Required]
-		public string last_name { get; set; }
-		[Required]
-		public string first_name { get; set; }
-		public string? middle_name { get; set; }
-		[Required]
-		[DataType(DataType.Date)]
-		public DateOnly birth_date { get; set; }
-		public string? email { get; set; }
-		public string? phone { get; set; }
-		public byte[]? photo { get; set; }
 		[Required]
 		[ForeignKey(nameof(Group))]
 		public int group { get; set; }
