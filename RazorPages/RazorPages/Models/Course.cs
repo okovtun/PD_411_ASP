6a8b16a13f830a925c91fec6c8a9c431a6b9dcd6
User <User@DESKTOP-QHG18FL>
2026-04-20 15:22:59 +0300

    RazorPages ContosoUniversity start

---
+++ RazorPages/RazorPages/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPages.Models
+{
+	public class Course
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public int CourseId { get; set; }
+		public string Title { get; set; }
+		public string Credits { get; set; }
+
+		//Navigation properties:
+		public ICollection<Enrollment> Enrollments { get; set; }
+	}
+}
---
+++ RazorPages/RazorPages/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPages.Models
+{
+	public class Student
+	{
+		public int Id { get; set; }
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+		public DateTime EnrollmentDate { get; set; }
+
+		//Navigation properties:
+		public ICollection<Enrollment> Enrollments { get; set; }
+	}
+}
