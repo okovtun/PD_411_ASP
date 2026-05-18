efbdf8fcc815afb222d8d15da8682b45ad46ec21
User <User@DESKTOP-QHG18FL>
2026-05-18 15:45:58 +0300

    Concurrency tokents handle Edit DONE

--- RazorPages/RazorPages/Pages/Departments/Edit.cshtml.cs
+++ RazorPages/RazorPages/Pages/Departments/Edit.cshtml.cs
@@ -6,43 +6,50 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using RazorPages.Data;
 using RazorPages.Models;
 
 namespace RazorPages.Pages.Departments
 {
-    public class EditModel : PageModel
-    {
-        private readonly RazorPages.Data.ContosoUniversityContext _context;
+	public class EditModel : PageModel
+	{
+		private readonly RazorPages.Data.ContosoUniversityContext _context;
 
-        public EditModel(RazorPages.Data.ContosoUniversityContext context)
-        {
-            _context = context;
-        }
+		public EditModel(RazorPages.Data.ContosoUniversityContext context)
+		{
+			_context = context;
+		}
 
-        [BindProperty]
-        public Department Department { get; set; } = default!;
+		[BindProperty]
+		public Department Department { get; set; } = default!;
+		public SelectList InstructorNameSL { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		public async Task<IActionResult> OnGetAsync(int id)
+		{
+			Department = await _context.Departments
+								.Include(d => d.Administrator)
+								.AsNoTracking()
+								.FirstOrDefaultAsync(m => m.DepartmentID == id);
+			//if (id == null)
+			//{
+			//    return NotFound();
+			//}
 
-            var department =  await _context.Departments.FirstOrDefaultAsync(m => m.DepartmentID == id);
-            if (department == null)
-            {
-                return NotFound();
-            }
-            Department = department;
-           ViewData["InstructorID"] = new SelectList(_context.Instructors, "ID", "FirstName");
-            return Page();
-        }
+			//var department =  await _context.Departments.FirstOrDefaultAsync(m => m.DepartmentID == id);
+			if (Department == null)
+			{
+				return NotFound();
+			}
+			//this.Department = Department;
+			InstructorNameSL = new SelectList(_context.Instructors, "ID", "FirstName");
+			//ViewData["InstructorID"] = new SelectList(_context.Instructors, "ID", "FirstName");
+			return Page();
+		}
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more information, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more information, see https://aka.ms/RazorPagesCRUD.
+		/*public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
             {
@@ -68,11 +75,82 @@ namespace RazorPages.Pages.Departments
             }
 
             return RedirectToPage("./Index");
-        }
+        }*/
+		public async Task<IActionResult> OnPostAsync(int id)
+		{
+			if (!ModelState.IsValid) return Page();
 
-        private bool DepartmentExists(int id)
-        {
-            return _context.Departments.Any(e => e.DepartmentID == id);
-        }
-    }
+			Department department = await _context.Departments
+											.Include(d => d.Administrator)
+											.FirstOrDefaultAsync(m => m.DepartmentID == id);
+			//TODO:
+			if (department == null) return HandleDeletedDepartment();
+			_context.Entry(department)
+					.Property(d => d.ConcurrencyToken)
+					.OriginalValue = this.Department.ConcurrencyToken;
+
+			bool success = await TryUpdateModelAsync<Department>
+				(
+				department,
+				"Department",
+				d => d.Name, d => d.StartDate, d => d.Budget, d => d.InstructorID
+				);
+			if (success)
+			{
+				try
+				{
+					await _context.SaveChangesAsync();
+					return RedirectToPage("./Index");
+				}
+				catch (DbUpdateConcurrencyException ex)
+				{
+					EntityEntry exceptionEntry = ex.Entries.Single();
+					Department clientValues = exceptionEntry.Entity as Department;
+					PropertyValues databaseEntry = exceptionEntry.GetDatabaseValues();
+					if (databaseEntry == null)
+					{
+						ModelState.AddModelError(string.Empty, "Невозможно сохранить, Department был удален");
+						return Page();
+					}
+					Department dbValues = databaseEntry.ToObject() as Department;
+					//TODO:
+					await SetDbErrorMessage(dbValues, clientValues, _context);
+					this.Department.ConcurrencyToken = dbValues.ConcurrencyToken;
+					ModelState.Remove($"{nameof(Department)}.{nameof(Department.ConcurrencyToken)}");
+				}
+			}
+			InstructorNameSL = new SelectList(_context.Instructors, "ID", "FullName", department.InstructorID);
+			return Page();
+		}
+		IActionResult HandleDeletedDepartment()
+		{
+			ModelState.AddModelError
+				(
+					string.Empty,
+					"HandleDeletedDepartment: Департамент был удален другим пользователем"
+				);
+			InstructorNameSL = new SelectList(_context.Instructors, "ID", "FullName", Department.InstructorID);
+			return Page();
+		}
+		async Task SetDbErrorMessage(Department dbValues, Department clientValues, ContosoUniversityContext context)
+		{
+			if (dbValues.Name != clientValues.Name) ModelState.AddModelError("Department.Name", $"Current value:{dbValues.Name}");
+			if (dbValues.Budget != clientValues.Budget) ModelState.AddModelError("Department.Budget", $"Current value:{dbValues.Budget:c}");
+			if (dbValues.StartDate != clientValues.StartDate) ModelState.AddModelError("Department.StartDate", $"Current value:{dbValues.StartDate:d}");
+
+			if (dbValues.InstructorID != clientValues.InstructorID)
+			{
+				Instructor dbInstructor = await _context
+												.Instructors
+												.FindAsync(dbValues.InstructorID);
+				ModelState.AddModelError("Department.InstructorID", $"Current value:{dbInstructor.FullName}");
+			}
+			ModelState.AddModelError(string.Empty, "Error:Запись, которую Вы пытаетесь редактировать, была изменена другим юзверем, операция была отклонена. Если Вы все еще хотите сохранить изменения, нажимайте на Сову");
+		}
+
+		private bool DepartmentExists(int id)
+		{
+			return _context.Departments.Any(e => e.DepartmentID == id);
+		}
+	}
 }
---
+++ RazorPages/RazorPages/Utility.cs
@@ -0,0 +1,10 @@
+﻿namespace RazorPages
+{
+	public static class Utility
+	{
+		public static string GetLastChars(byte[] token)
+		{ 
+			return token[7].ToString();
+		}
+	}
+}
